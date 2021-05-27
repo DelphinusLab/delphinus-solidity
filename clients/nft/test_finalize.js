9c8e1580832c1e03f27287dbe8d6f64e035406c6
akayi07 <zhuolimiaotx@163.com>
2021-05-27 17:27:42 +0800

    test the method finalize

--- clients/nft/test_finalize.js
+++ clients/nft/test_finalize.js
@@ -11,49 +11,66 @@ async function test_finalize(id, price) {
   let account = Config.monitor_account;
   let nft_client = new nft.NftClient(web3, Config, TokenInfo, NFTInfo, BiddingInfo, account);
   try { 
-    await nft_client.mint(id);
-    const token = await nft_client.token; 
-    const balance_before = await token.methods.balanceOf(account).call();
-    if(balance_before < price) {
+    await nft_client.mint(id); // mint before auction 
+    var token = await nft_client.token; 
+    var balance_before = await token.methods.balanceOf(account).call(); // get balance before auction
+    console.log("balance_before:", balance_before);
+    if(balance_before < price) { // the price must be greater than the balance
       console.log("balance is not enough!");
       return false;
-    }  
-    await nft_client.auction(id, price-1);
-    let bid_info_before = await nft_client.getAuctionInfo(id);
-    if(Number(bid_info_before.price) == 0) {
-      console.log("Please auction at first")
+    }
+    let auction_info_before = await nft_client.getAuctionInfo(id); 
+    console.log("auction_before", auction_info_before)
+    if(auction_info_before.owner != 0) {  // owner should equal to zero 
+        console.log("auction has already succeed");
+        return false;
+    }
+    await nft_client.auction(id, price-1); // auction before bid & the bid price must be higher than the auction 
+    auction_info_after = await nft_client.getAuctionInfo(id);
+    console.log("auction_after:",auction_info_after);
+    if (Number(auction_info_after.price) != price-1) { // the price should be updated
+      console.log("auction failed");
       return false;
     }
-    if(price <= Number(bid_info_before.price)) {
-      console.log("bid:price is lower");
-      return false; 
-    }
-    await nft_client.bid(id,price);
-    let bid_info_after = await nft_client.getAuctionInfo(id);
-    if(Number(bid_info_after.price) > Number(bid_info_before.price)) {  
+    var alreadyBid = true;
+    await nft_client.bid(id,price); // bid before finalize
+    bid_info_after = await nft_client.getAuctionInfo(id); 
+    console.log("bid_after", bid_info_after)
+    if(Number(bid_info_after.price) > Number(auction_info_after.price)) { //the price shoud be updated  
       const balance_after = await token.methods.balanceOf(account).call();
-      if(balance_before-balance_after != price) {
+      console.log("balance_after:", balance_after);
+      if(balance_before-balance_after != price) { // The number of reductions should be equal to price
         console.log("bid:current balance is not correct!");
         return false;
       }
     } else {
-      console.log("bid failed");
+      console.log("bid:bid failed!");
       return false;
     }
-
-    let auction_before_finalize = await nft_client.getAuctionInfo(id);
-    console.log("auction_before_finalize:",auction_before_finalize);
+    let owner_before_finalize = await nft_client.getOwner(id);
+    console.log("owner_before_finalize",owner_before_finalize);
     await nft_client.finalize(id);
-    
-    let auction_after_finalize = await nft_client.getAuctionInfo(id);
-    console.log("auction_after_finalize:", auction_after_finalize);
-    if(auction_after_finalize.price == 0 && auction_after_finalize.owner == 0 &&  auction_after_finalize.winner == 0) {
-      return true;
+    let owner_after_finalize = await nft_client.getOwner(id);
+    console.log("owner_after_finalize:", owner_after_finalize); 
+    let auctionInfo_after_finalize = await nft_client.getAuctionInfo(id);
+    console.log("auctionInfo_after_finalize:", auctionInfo_after_finalize);
+    if(auctionInfo_after_finalize.price == 0 && auctionInfo_after_finalize.owner == 0 &&  auctionInfo_after_finalize.winner == 0) {
+      if(owner_after_finalize == owner_before_finalize) { // owner should be updated
+        console.log("owner did not updated");
+        return false;
+      } else {
+        return true;
+      }
     } else {
       console.log("finalize failed")
       return false;
     }
-   } catch (error) {
+   } catch (error) { 
+     let current_balance = await token.methods.balanceOf(account).call();
+     if(current_balance != balance_before && alreadyBid) { // if bid failed, balance should not decrease
+       console.log("bid:balance decreased");
+       return false;
+     }
      let msg = error.message.split(':')
      console.log(msg[msg.length - 1]);
      return false;
@@ -69,6 +86,7 @@ function check_finalize(result, indicator) {
 }
 
 async function finalize() {
+  await test_finalize(0x256, 6).then(v => {check_finalize(v, true);});
   await test_finalize(0x233, 6).then(v => {check_finalize(v, true);});
   await test_finalize(0x234, 5).then(v => {check_finalize(v, true);});
   await test_finalize(0x235, 5).then(v => {check_finalize(v, true);});
