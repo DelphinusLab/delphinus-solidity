08a579549db6e72c9196af8fc877f7b22df7ced2
xgao@zoyoe.com <xgao@zoyoe.com>
2021-05-29 23:45:31 +0800

    add bsctestnet and ethtestnet

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,14 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
+const priv_key = fs.readFileSync("tools/key.prv").toString().trim();
+
+const infura_id = '1c8e4178f8954e01a95c8eef7b8af2b7';
 
 module.exports = {
   /**
@@ -44,40 +47,43 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      //host: "114.119.187.230",     // Localhost (default: none)
-      //port: 30645,            // Standard Ethereum port (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      from: "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b",
+      gas: 3100000,           // Gas sent with each transaction (default: ~6700000)
+      websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    },
+    testnet1: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      from: "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b",
+      gas: 3100000,           // Gas sent with each transaction (default: ~6700000)
+      websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    },
+    testnet2: {
+      host: "127.0.0.1",     // Localhost (default: none)
       port: 8745,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
       from: "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b",
       gas: 3100000,           // Gas sent with each transaction (default: ~6700000)
       websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(priv_key, "https://ropsten.infura.io/v3/" + infura_id),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    bsctestnet: {
+      provider: () => new HDWalletProvider(priv_key, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
     }
-    //,
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -89,14 +95,6 @@ module.exports = {
   compilers: {
     solc: {
       version: "native",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
     }
   },
 
