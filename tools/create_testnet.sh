8caca77bdfac5cc0148558de26948560405eaa1e
Shindar <shindar90@gmail.com>
2021-05-29 12:27:00 +0800

    scripts: unify testnet create

--- tools/create_testnet.sh
+++ tools/create_testnet.sh
@@ -1,8 +1,11 @@
-DATA_DIR=$1
 ADDRESS="0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b"
-mkdir ${DATA_DIR}1
-geth account import --datadir ${DATA_DIR}1 key.prv
-geth --datadir ${DATA_DIR}1 init genesis1.json
-mkdir ${DATA_DIR}2
-geth account import --datadir ${DATA_DIR}2 key.prv
-geth --datadir ${DATA_DIR}2 init genesis2.json
+PASSWORD=123
+
+echo $PASSWORD > .password
+
+mkdir testnet1.backup
+geth account import --datadir testnet1.backup key.prvgeth --datadir testnet1.backup init genesis1.json
+
+mkdir testnet2.backup
+geth account import --datadir testnet2.backup key.prv
+geth --datadir testnet1.backup init genesis2.json
