0f436759fcc277f90e3d46cd5d3a631d677c9b80
xgao@zoyoe.com <xgao@zoyoe.com>
2021-05-30 22:40:48 +0800

    fix: using hdwalletprovider

--- clients/config.js
+++ clients/config.js
@@ -1,4 +1,4 @@
-const PrivateKeyProvider = require('web3-privatekey-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
 const infura_id = fs.readFileSync(__dirname + "/../.secret.infura").toString().trim();
 let priv_key = "0x" + fs.readFileSync(__dirname + "/../tools/key.prv").toString().trim();
@@ -24,7 +24,7 @@ module.exports = {
     device_id: "16",
   },
   bsctestnet: {
-    provider: () => new PrivateKeyProvider(priv_key, `https://bsc.getblock.io/testnet/?api_key=182a8e0d-c03a-44ac-b856-41d2e47801db`),
+    provider: () => new HDWalletProvider(priv_key, `https://bsc.getblock.io/testnet/?api_key=182a8e0d-c03a-44ac-b856-41d2e47801db`),
     mongodb_url: "mongodb://localhost:27017",
     web3_source: "wss://bsc.getblock.io/testnet/?api_key=182a8e0d-c03a-44ac-b856-41d2e47801db",
     monitor_account: "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b",
@@ -32,7 +32,7 @@ module.exports = {
     device_id: "97",
   },
   ropsten: {
-    provider: () => new PrivateKeyProvider(priv_key, "https://ropsten.infura.io/v3/" + infura_id),
+    provider: () => new HDWalletProvider(priv_key, "https://ropsten.infura.io/v3/" + infura_id),
     mongodb_url: "mongodb://localhost:27017",
     web3_source: "wss://ropsten.infura.io/ws/v3/" + infura_id,
     monitor_account: "0x6f6ef6dfe681b6593ddf27da3bfde22083aef88b",
