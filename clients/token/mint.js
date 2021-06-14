9f023dfc5009012f01d0f1b45f554fd7baa7cff2
xgao@zoyoe.com <xgao@zoyoe.com>
2021-06-15 00:22:20 +0800

    trial of new pbinder in web3subscriber

--- clients/token/mint.js
+++ clients/token/mint.js
@@ -2,25 +2,29 @@ const Web3 = require("web3")
 const FileSys = require("fs")
 const Config = require("../config")
 const Client = require("web3subscriber/client")
+const PBinder= require("web3subscriber/pbinder")
 const TokenInfo = require("../../build/contracts/Token.json")
 
-
-async function test_mint(config_name) {
-  config = Config[config_name];
-  let account = config.monitor_account;
-  let web3 = await Client.initWeb3(config, false);
-  let token= Client.getContract(web3, config, TokenInfo, account);
-  try {
-    var balance = await Client.getBalance(token, account);
-    console.log("balance before mint:", balance);
-    await token.methods.mint(0x100).send();
-    balance = await Client.getBalance(token, account);
-    console.log("balance after mint", balance);
-  } catch (err) {
-    console.log("%s", err);
-  }
+function test_mint(config_name) {
+  let pbinder = new PBinder.PromiseBinder();
+  let r = pbinder.return (async () => {
+    config = Config[config_name];
+    let account = config.monitor_account;
+    let web3 = await Client.initWeb3(config, false);
+    let token= Client.getContract(web3, config, TokenInfo, account);
+    try {
+      var balance = await Client.getBalance(token, account);
+      console.log("balance before mint:", balance);
+      await pbinder.bind("mint", token.methods.mint(0x100).send());
+      balance = await Client.getBalance(token, account);
+      console.log("balance after mint", balance);
+    } catch (err) {
+      console.log("%s", err);
+    }
+  });
+  return r;
 }
 
-
+/* .once("transactionHash",hash => console.log(hash) */
 console.log(Config[process.argv[2]]);
-test_mint(process.argv[2]).then(v => {console.log("test done!"); process.exit();});
+test_mint(process.argv[2]).when("mint","transactionHash", hash=>console.log(hash)).then(v => {console.log("test done!"); process.exit();});
