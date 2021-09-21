1f15cb42eb561eecb21e41c40ced8aa4a7f5585e
Xin Gao <xgao@zoyoe.com>
2021-09-21 20:00:46 +0800

    cleanup: remove uncessary js files in client

--- clients/utils.js
+++
@@ -1,9 +0,0 @@
-function append_info(c) {
-  let ele = document.createElement("p");
-  ele.appendChild(document.createTextNode(c));
-  document.body.appendChild(ele);
-}
-
-module.exports = {
-  info: append_info,
-}
