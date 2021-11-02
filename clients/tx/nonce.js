66ffac77479a67c2d92f46691ad9719ff2c699c8
zoyoe <zkp@zoyoe.com>
2021-11-02 21:56:00 +0800

    add tx for clients

---
+++ clients/tx/nonce.js
@@ -0,0 +1,35 @@
+const Web3 = require("web3")
+const FileSys = require("fs")
+const Config = require("../config")
+const Client = require("web3subscriber/client")
+const PBinder= require("web3subscriber/pbinder")
+const TokenInfo = require("../../build/contracts/Token.json")
+const Secrets = require('../../.secrets');
+const BridgeABI = require("../bridge/abi");
+
+function tx_info(config_name1, config_name2) {
+  let pbinder = new PBinder.PromiseBinder();
+  let config1 = Config[config_name1](Secrets);
+  let config2 = Config[config_name2](Secrets);
+  let r = pbinder.return (async () => {
+    config1 = Config[config_name1](Secrets);
+    config2 = Config[config_name2](Secrets);
+    let account = config1.monitor_account;
+    let web31 = await Client.initWeb3(config1, false);
+    let web32 = await Client.initWeb3(config2, false);
+    console.log("version:", web31.version);
+    let tx_count2 = await web31.eth.getTransactionCount(account);
+    let tx_count_pending2 = await web31.eth.getTransactionCount(account, "pending");
+    console.log("Transaction count is ", tx_count2, tx_count_pending2);
+    let tx_count3 = await web32.eth.getTransactionCount(account);
+    let tx_count_pending3 = await web32.eth.getTransactionCount(account, "pending");
+    console.log("Transaction count is ", tx_count3, tx_count_pending3);
+    let id = await web32.eth.net.getId();
+    console.log(id);
+
+  });
+  return r;
+}
+
+/* .once("transactionHash",hash => console.log(hash) */
+tx_info(process.argv[2], process.argv[3]).then(v => {console.log("test done!"); process.exit();});
---
+++ clients/tx/tx.js
@@ -0,0 +1,22 @@
+const Web3 = require("web3")
+const FileSys = require("fs")
+const Config = require("../config")
+const Client = require("web3subscriber/client")
+const PBinder= require("web3subscriber/pbinder")
+const TokenInfo = require("../../build/contracts/Token.json")
+const Secrets = require('../../.secrets');
+
+function tx_info(config_name, transaction_hash) {
+  let pbinder = new PBinder.PromiseBinder();
+  let r = pbinder.return (async () => {
+    config = Config[config_name](Secrets);
+    let account = config.monitor_account;
+    let web3 = await Client.initWeb3(config, false);
+    let tx = await web3.eth.getTransaction(transaction_hash);
+    console.log("balance of recipient after transfer", tx);
+  });
+  return r;
+}
+
+/* .once("transactionHash",hash => console.log(hash) */
+tx_info(process.argv[2], process.argv[3]).then(v => {console.log("test done!"); process.exit();});
