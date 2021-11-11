7d290cb55dd747a65723ee6dba287b8f23cbd992
Zhang Junyu <junyu92@gmail.com>
2021-11-11 16:31:26 +0800

    refactor bridge with typescript
    Signed-off-by: Zhang Junyu <junyu92@gmail.com>

---
+++ clients/types/bridge.ts
@@ -0,0 +1,12 @@
+export interface BridgeInfo {
+    chain_id: string;
+    amount_token: string;
+    amount_pool: string;
+    owner: string;
+    merkle_root: string;
+    rid: string;
+  }
+
+  export interface TokenInfo {
+    token_uid: string;
+  }
