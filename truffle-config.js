37630e7218720b386bec3e453bcd5b30dce2388c
akayi07 <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>
2023-01-31 07:23:18 +0800

    replace cronostestnet with cantotestnet

--- truffle-config.js
+++ truffle-config.js
@@ -100,10 +100,10 @@ module.exports = {
       timeoutBlocks: 200,
       skipDryRun: true
     },
-    cronostestnet: {
+    cantotestnet: {
       provider: () => new HDWalletProvider(secrets.accounts.deployer.priv,
-        "wss://cronos-testnet-3.crypto.org:8546"),
-      network_id: 338,
+        http_provider("https://eth.plexnode.wtf/")),
+      network_id: 740,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true
